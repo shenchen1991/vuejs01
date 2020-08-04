0e9f44cbb581da982673225e133f906dff77f140
shenchen <572631358@qq.com>
2020-08-04 18:39:29 +0800

    更新

---
+++ 17-vuex/01-learnVuex/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h2>{{message}}</h2>
+    <h1>{{counter}}</h1>
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+    <hello-vuex :counter = "counter"></hello-vuex>
+  </div>
+</template>
+
+<script>
+import HelloVuex from "./components/HelloVuex";
+
+export default {
+  name: 'App',
+  components:{
+    HelloVuex
+  },
+  data(){
+    return {
+      message:'我是传奇',
+      counter:0
+    }
+  }
+}
+</script>
+
+<style>
+</style>
